c87a52910c967cc5a9c780bdbe17eae08cb9f6e9
Tomas Basham <tomasbasham@gmail.com>
2016-01-10 17:18:12 +0000

    Initial commit

---
+++ arm-none-eabi/lib/gcc/arm-none-eabi/4.9.2/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../gcc-4.9.2/configure --target=arm-none-eabi --prefix=/opt/wookie_tools/arm-none-eabi --disable-nls --enable-languages=c,c++ --without-headers";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { NULL, NULL} };
---
+++ i686-elf/lib/gcc/i686-elf/4.9.2/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../gcc-4.9.2/configure --target=i686-elf --prefix=/opt/wookie_tools/i686-elf --disable-nls --enable-languages=c,c++ --without-headers";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "cpu", "generic" }, { "arch", "pentiumpro" } };
